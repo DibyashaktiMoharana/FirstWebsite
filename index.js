f8fa0a39781310ae225ec06f32bf90febeaad1f5
DibyashaktiMoharana <129984256+DibyashaktiMoharana@users.noreply.github.com>
2023-04-16 12:30:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+$(".form").hide();
+
+$(".inputdropdownbtn").on("click",function(){
+    $(".form").slideToggle();
+});
+
+
+    // sahi hai apna
+$(document).onload(function (){
+    $("textarea").text("Type your message here!");
+});
